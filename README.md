7cc03a9f66170d6cfa0737479bb13b5c4ad38c33
Eric J Nesser <enesser@gmail.com>
2015-08-27 01:53:03 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # alice
+
+    | Description           | Windows     | Mac                | HTML
+--- | --------------------- | ----------- | ------------------ | -----
+'   | straight single quote | '           | '                  | '
+"   | straight double quote | "           | "                  | "
+‘   | opening single quote  | alt + 0145  | option + ]         | &lsquo;
+’   | closing single quote  | alt + 0146  | option + shift + ] | &rsquo;
+“   | opening double quote  | alt + 0147  | option + [         | &ldquo;
+”   | closing double quote  | alt + 0148  | option + shift + [ | &rdquo;
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+/**
+ * Path to assets folder where you'll author CSS, JavaScript assets
+ * @type {String}
+ */
+var assetsFolderPath = 'assets';
+
+/**
+ * Styles folder path to styles processed by gulp; this folder will auto-generate
+ * @type {String}
+ */
+var stylesFolderPath = 'styles';
+
+/**
+ * Source paths where you'll author CSS, JavaScript
+ * @type {Object}
+ */
+var sourcePaths = {
+    sass: [assetsFolderPath + '/**/*.scss']
+};
+
+/**
+ * Destination paths where assets will go after they're built, do not edit files in this
+ * directory directly
+ * @type {Object}
+ */
+var destinationPaths = {
+    styles: stylesFolderPath
+};
+
+// gulp sass
+gulp.task('sass', function() {
+    gulp.src(sourcePaths.sass)
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest(destinationPaths.styles));
+});
+
+// gulp watch
+gulp.task('watch', function() {
+    gulp.watch(sourcePaths.sass, ['sass']);
+});
+
+// default gulp task
+gulp.task('default', ['sass']);
